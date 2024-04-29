61c1a0bde86e53497855e05ef400da983328328a
danielbrai <daniel.gm.brai@gmail.com>
2024-04-28 22:31:19 -0300

    feat: validate input has only one element.

--- src/main/java/com/danielbrai/VectorSumChallenge.java
+++ src/main/java/com/danielbrai/VectorSumChallenge.java
@@ -3,6 +3,11 @@ package com.danielbrai;
 public class VectorSumChallenge {
 
     public int[] executeAlgorithm(int[] input) {
+
+        if (input.length < 2) {
+            return new int[0];
+        }
+
         return null;
     }
 }
--- src/test/java/com/danielbrai/VectorSumChallengeTest.java
+++ src/test/java/com/danielbrai/VectorSumChallengeTest.java
@@ -1,7 +1,16 @@
 package com.danielbrai;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class VectorSumChallengeTest {
 
-
+    @Test
+    public void shouldReturnEmptyArrayIfTheInputHasOnlyOneElement() {
+        int[] input = {1};
+        VectorSumChallenge vectorSumChallenge = new VectorSumChallenge();
+        int[] ints = vectorSumChallenge.executeAlgorithm(input);
+        Assertions.assertEquals(0, ints.length);
+    }
 
 }
